7b06187521558227be11cb00d958b160634890dd
Nazim Lachter <lachtater@gmail.com>
2020-04-21 13:39:59 +0200

    images per language

--- src/core/components/Image.tsx
+++ src/core/components/Image.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
+import { LanguagesObject_IncludeAll, i18nSettings } from "dropin-recipes"
 import { ComponentProps, Component } from "./Component"
 
 interface ImageProps extends ComponentProps {
-  source: string
+  source: string|LanguagesObject_IncludeAll<string>
   alt?: string
 }
 
@@ -10,7 +11,17 @@ export class Image extends Component<ImageProps> {
 
   render() {
     const { source } = this.props
-    return <img src={source} alt={this.props.alt} style={this.state.style}/>
+    let finalSource = ""
+    if(typeof source === "object") {
+      if(typeof source[i18nSettings.getCurrentLanguage()] !== "undefined") {
+        finalSource = source[i18nSettings.getCurrentLanguage()]
+      } else if(typeof source[i18nSettings.getDefaultLanguage()] !== "undefined") {
+        finalSource =  source[i18nSettings.getDefaultLanguage()]
+      }
+    } else {
+      finalSource = source
+    }
+    return <img src={finalSource} alt={this.props.alt} style={this.state.style}/>
   }
 
 }
--- src/core/components/Video.tsx
+++ src/core/components/Video.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
+import { LanguagesObject_IncludeAll, i18nSettings } from "dropin-recipes"
 import { ComponentProps, Component } from "./Component"
 
 interface VideoProps extends ComponentProps {
-  sources: string[]
+  sources: string[]|LanguagesObject_IncludeAll<string[]>
   keyPrefix: string
 }
 
@@ -10,8 +11,21 @@ export class Video extends Component<VideoProps> {
 
   render() {
     const { sources, keyPrefix } = this.props
+
+    let finalSources: string[] = []
+    if(typeof sources === "object") {
+      const sourcesAsLanguages = sources as LanguagesObject_IncludeAll<string[]>
+      if(typeof sourcesAsLanguages[i18nSettings.getCurrentLanguage()] !== "undefined") {
+        finalSources = sourcesAsLanguages[i18nSettings.getCurrentLanguage()]
+      } else if(typeof sourcesAsLanguages[i18nSettings.getDefaultLanguage()] !== "undefined") {
+        finalSources =  sourcesAsLanguages[i18nSettings.getDefaultLanguage()]
+      }
+    } else {
+      finalSources = sources
+    }
+
     return <video style={this.state.style} loop autoPlay>
-      {sources.map((source, index) => {
+      {finalSources.map((source, index) => {
         const split = source.split(".")
         const format = split[split.length - 1]
         return <source key={`${keyPrefix}-${index}`} src={source} type={`video/${format}`}/>
