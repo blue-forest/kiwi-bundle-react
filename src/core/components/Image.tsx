52c83cd3c72b6b22570000a591cfaa2385b4f0ab
Nazim Lachter <lachtater@gmail.com>
2020-04-24 20:51:59 +0200

    Shorter components

--- src/core/components/Image.tsx
+++ src/core/components/Image.tsx
@@ -1,27 +1,32 @@
 import * as React from "react"
-import { LanguagesObject_IncludeAll, i18nSettings } from "dropin-recipes"
+import { LanguagesObject_IncludeAll, i18nSettings, i18nData, i18n, i18nSchema } from "dropin-recipes"
 import { ComponentProps, Component } from "./Component"
 
 interface ImageProps extends ComponentProps {
   source: string|LanguagesObject_IncludeAll<string>
-  alt?: string
+  alt?: string|i18nSchema
 }
 
 export class Image extends Component<ImageProps> {
 
   render() {
-    const { source } = this.props
-    let finalSource = ""
-    if(typeof source === "object") {
-      if(typeof source[i18nSettings.getCurrentLanguage()] !== "undefined") {
-        finalSource = source[i18nSettings.getCurrentLanguage()]
-      } else if(typeof source[i18nSettings.getDefaultLanguage()] !== "undefined") {
-        finalSource =  source[i18nSettings.getDefaultLanguage()]
-      }
-    } else {
-      finalSource = source
-    }
-    return <img src={finalSource} alt={this.props.alt} style={this.state.style}/>
+    const { source, alt } = this.props
+    return <img
+      src={(() => {
+        if(typeof source === "string") {
+          return source
+        }
+        if(typeof source[i18nSettings.getCurrentLanguage()] !== "undefined") {
+          return source[i18nSettings.getCurrentLanguage()]
+        }
+        if(typeof source[i18nSettings.getDefaultLanguage()] !== "undefined") {
+          return source[i18nSettings.getDefaultLanguage()]
+        }
+        return ""
+      })()}
+      alt={typeof alt === "string" ? alt : i18n(alt as i18nSchema)}
+      style={this.state.style}
+    />
   }
 
 }
--- src/core/components/Text.tsx
+++ src/core/components/Text.tsx
@@ -1,26 +1,27 @@
 import * as React from "react"
-import { i18nData, i18nSettings, i18n, XOR } from "dropin-recipes"
+import { i18nData, i18nSettings, i18n, XOR, i18nSchema } from "dropin-recipes"
 import { ComponentProps, Component } from "./Component"
 
 type TextProps = ComponentProps & XOR<{
   id: string
-  children: i18nData
+  children: i18nSchema
 }, {
   children: string
 }>
 
 export class Text extends Component<TextProps> {
 
-  renderChildren() {
-    const { children, id } = this.props
-    if(typeof children === "object") {
-      return i18nSettings.compileMarkdown<React.ReactElement>(id as string, i18n(children as i18nData))
-    }
-    return children
-  }
-
   render() {
-    return <span style={this.state.style} children={this.renderChildren()}/>
+    const { children, id } = this.props
+    return <span
+      style={this.state.style}
+      children={(() => {
+        if(typeof children === "object") {
+          return i18nSettings.compileMarkdown<React.ReactElement>(id as string, i18n(children))
+        }
+        return children
+      })()}
+    />
   }
 
 }
