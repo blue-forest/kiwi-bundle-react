d33e8c86d39807b16930defe564faa21244d5933
Nazim Lachter <nazim@blueforest.cc>
2020-06-25 17:24:31 +0200

    Revert "Exported KB"
    This reverts commit 252910706af65d148ba702187cd2facb192a13d5.

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -1,4 +1,4 @@
-import { chalk } from "kiwi-bundle"
+import chalk from "chalk"
 
 export const webpackConsoleLog = (text: string) => {
   console.log(`${chalk.blue("ℹ")} ${chalk.gray("｢kwb｣")}: ${text}`)
