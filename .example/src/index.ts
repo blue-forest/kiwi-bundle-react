843ae560b613ebd58b519e7f48b0f41bf8e7e3af
Nazim Lachter <nazim@blueforest.cc>
2020-09-05 00:20:55 +0200

    Exemple React Native files

---
+++ .example/src/index.ts
@@ -0,0 +1,6 @@
+import { KBS } from "./bundle"
+import { HomePage } from "./pages/Home"
+
+KBS.Render({
+  HOME: HomePage,
+})
---
+++ .example/src/vendors/kiwi-bundle-react/index.ts
@@ -0,0 +1,4 @@
+import * as React from "react"
+import * as Kiwi from "react-native"
+
+export { React, Kiwi }
