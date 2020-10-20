c7c6bda78e19d62956318415a01c38cf4d24c9c1
Nazim Lachter <nazim@blueforest.fr>
2020-10-20 17:32:57 +0200

    cross platform execution

--- src/handlers/utils.ts
+++ src/handlers/utils.ts
@@ -1,18 +1,20 @@
 import { spawn } from "child_process"
-import { join as pathJoin } from "path"
+import { join } from "path"
 
-export const run = (path: string, bin: string, args: string[]) => spawn(
-  pathJoin(path, "node_modules/.bin", bin),
-  args,
-  {
-    // stdio: [ process.stdin, process.stdout, process.stderr ],
-    shell: true,
-    stdio: "inherit",
-    cwd: path,
-    env: {
-      ...process.env,
-      SKIP_PREFLIGHT_CHECK: "true",
-      FORCE_COLOR: "true",
-    },
-  }
-)
+export const run = (path: string, bin: string, args: string[]) => {
+  return spawn(
+    join("node_modules", ".bin", bin),
+    args,
+    {
+      // stdio: [ process.stdin, process.stdout, process.stderr ],
+      shell: true,
+      stdio: "inherit",
+      cwd: path,
+      env: {
+        ...process.env,
+        SKIP_PREFLIGHT_CHECK: "true",
+        FORCE_COLOR: "true",
+      },
+    }
+  )
+}
