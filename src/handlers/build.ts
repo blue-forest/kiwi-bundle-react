896628aed044bde28eb93db3c201e27c11f840fb
Nazim Lachter <nazim@blueforest.cc>
2020-11-02 18:26:28 +0100

    updates & fixes

--- src/handlers/build.ts
+++ src/handlers/build.ts
@@ -4,9 +4,9 @@ import { spawn } from "child_process"
 export const main: KiwiBundleHandler = ({ path }) => {
   spawn(
     "./node_modules/.bin/react-scripts",
-    [ "build" ],
+    ["build"],
     {
-      // shell: true,
+      shell: true,
       stdio: "inherit",
       uid: 1000,
       cwd: path,
