20efb4d3962ac41226b9d32dd0e23ac557854d93
Nazim Lachter <nazim@blueforest.cc>
2020-11-22 03:58:08 +0100

    Starting responsive styles

--- demo/src/kbrd/pages/Styles.tsx
+++ demo/src/kbrd/pages/Styles.tsx
@@ -1,22 +1,28 @@
 import { React, Kiwi } from "kiwi-bundle-react"
 import { KBRD } from ".."
 import { ButtonComponent } from "../components/Button"
+import { StylesPageStyle } from "./Styles.style"
 
-export default KBRD.Page((self) => self.render(({ appearance }) => {
-  console.log({ type: "render", name: "Styles" })
-  const scheme = appearance.theme.scheme.get()
-  return <Kiwi.View>
-    <ButtonComponent
-      title={`Switch from ${scheme} to ${
-        scheme === "dark" ? "light" : "dark"
-      }`}
-      onPress={() => {
-        if (scheme === "dark") {
-          appearance.theme.scheme.set("light")
-        } else {
-          appearance.theme.scheme.set("dark")
-        }
-      }}
-    />
-  </Kiwi.View>
-}))
+export default KBRD.Page((self) =>
+  self
+  .style(StylesPageStyle)
+  .render(({ appearance, style }) => {
+    console.log({ type: "render", name: "StylesPage" })
+    const scheme = appearance.theme.scheme.get()
+    return <Kiwi.View>
+      <ButtonComponent
+        title={`Switch from ${scheme} to ${
+          scheme === "dark" ? "light" : "dark"
+        }`}
+        onPress={() => {
+          if (scheme === "dark") {
+            appearance.theme.scheme.set("light")
+          } else {
+            appearance.theme.scheme.set("dark")
+          }
+        }}
+      />
+      <Kiwi.View style={style.responsive}/>
+    </Kiwi.View>
+  })
+)
--- src/core/app/styles.ts
+++ src/core/app/styles.ts
@@ -15,3 +15,49 @@ export type StyleSheetMediaQuery = {
   max?: number
   style: StyleSheetStyle
 }
+
+export type AppStyleSheet<Style = StyleSheetStyle | StyleSheetMediaQuery[]> = {
+  [name: string]: Style
+}
+
+const windowDimensions = ReactNative.Dimensions.get("window")
+let WIDTH = windowDimensions.width
+let HEIGHT = windowDimensions.height
+//const ON_UPDATE: (() => void)[] = []
+
+const resized = () => {
+  console.log("UPDATE", WIDTH, HEIGHT)
+}
+
+ReactNative.Dimensions.addEventListener("change", ({ window }) => {
+  WIDTH = window.width
+  HEIGHT = window.height
+  resized()
+})
+
+export const renderStyle = (
+  style: AppStyleSheet,
+  set: (style: AppStyleSheet<StyleSheetStyle>) => void,
+  update: () => void,
+) => {
+  set(
+    Object.keys(style).reduce<AppStyleSheet<StyleSheetStyle>>(
+      (finalStyle, name) => {
+        const currentStyle = style[name]
+        if (Array.isArray(currentStyle)) {
+          finalStyle[name] = currentStyle.reduce<StyleSheetStyle>(
+            (all, current) => {
+              return Object.assign(all, current.style)
+            },
+            {},
+          )
+        } else {
+          finalStyle[name] = currentStyle
+        }
+        return finalStyle
+      },
+      {},
+    ),
+  )
+  console.log(style, update)
+}
