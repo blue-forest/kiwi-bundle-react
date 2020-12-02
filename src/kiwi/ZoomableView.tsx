ad5ced7289c0949a71f7fd5b670192db14db0394
Nazim Lachter <nazim@blueforest.cc>
2020-12-02 20:53:49 +0100

    Sandboxing

--- src/kiwi/ZoomableView.tsx
+++ src/kiwi/ZoomableView.tsx
@@ -1,10 +1,13 @@
-import { React } from "../vendors"
+import { React, ReactNative } from "../vendors"
 import { PinchGestureHandler } from "react-native-gesture-handler"
+import { StyleSheetStyleView } from "../core/app/styles"
 
 export interface ZoomableViewProps {
   children?: React.ReactNode
   onZoomUp?: () => void
   onZoomDown?: () => void
+  onMove?: (movement: { x: number; y: number }) => void
+  style?: ReactNative.StyleProp<StyleSheetStyleView>
 }
 
 export const ZoomableView = (props: ZoomableViewProps) => {
--- src/kiwi/ZoomableView.web.tsx
+++ src/kiwi/ZoomableView.web.tsx
@@ -4,7 +4,25 @@ import { ZoomableViewProps } from "./ZoomableView"
 export const ZoomableView = (props: ZoomableViewProps) => {
   const ref: any = React.createRef()
   React.useEffect(() => {
+    if (typeof props.onMove !== "undefined") {
+      ref.current.onmousedown = (downEvent: any) => {
+        if (downEvent.target === ref.current) {
+          const onMouseMove = (event: any) => {
+            props.onMove!({ x: event.movementX, y: event.movementX })
+          }
+          ref.current.addEventListener("mousemove", onMouseMove)
+          ref.current.onmouseup = () => {
+            ref.current.removeEventListener("mousemove", onMouseMove)
+            ref.current.onmouseup = null
+          }
+        }
+      }
+    }
+
+    ref.current.ondragstart = () => false
+
     ref.current.onwheel = (event: any) => {
+      //console.log(event.offsetX, event.offsetY)
       if (event.deltaY < 0) {
         if (typeof props.onZoomUp !== "undefined") {
           props.onZoomUp()
@@ -14,7 +32,10 @@ export const ZoomableView = (props: ZoomableViewProps) => {
           props.onZoomDown()
         }
       }
+      event.preventDefault()
     }
   }, [props, ref])
-  return <ReactNative.View ref={ref} children={props.children} />
+  return (
+    <ReactNative.View ref={ref} style={props.style} children={props.children} />
+  )
 }
